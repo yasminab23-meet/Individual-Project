80d15773892172eeea4d51b6b73fae2e54a06c8e
yasminab23-meet <109899977+yasminab23-meet@users.noreply.github.com>
2022-08-03 12:33:47 +0300

    Second session of working - Fixing errors and opening html files

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -27,18 +27,54 @@ def home():
     return render_template('index.html') 
     
 
-@app.route('/login')
+@app.route('/login',methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user']=auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('hobbies'))
+        except:
+            error='LOGIN FAILED'
     return render_template('login.html')
 
 
-@app.route('/create_account')
+@app.route('/create_account',methods=['GET', 'POST'])
 def create_account():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    try:
+        login_session['user']=auth.create_user_with_email_and_password(email, password)
+        return redirect(url_for('hobbies'))
+    except:
+        error='LOGIN FAILED'
     return render_template('create_account.html')
 
 @app.route('/hobbies')
 def hobbies():
     return render_template('hobbies.html')
+
+@app.route('/martial_arts')
+def martial_arts():
+    return render_template('martial_arts.html')
+
+@app.route ('/climbing')
+def climbing():
+    return render_template('climbing.html')
+
+@app.route('/woodwork')
+def woodwork():
+    return render_template('woodwork.html')
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
